eeb70f74fac1a38797a8a4d1cd9eccede3f4b76e
Johan Roxas <75343741+rickardo-roxas@users.noreply.github.com>
2023-03-04 12:33:10 +0800

    Created new methods

--- src/prog2/midterm/MixedFraction.java
+++ src/prog2/midterm/MixedFraction.java
@@ -18,13 +18,13 @@ import prog2.prelim.Fraction;
 public class MixedFraction extends Fraction {
     private int wholeNumber; // holds the whole number for MixedFraction
 
-    public MixedFraction(int numerator, int denominator) {
-        super(numerator, denominator);
+    public MixedFraction(Fraction fraction) {
+        super(fraction.getNumerator(), fraction.getDenominator());
         wholeNumber = 0;
     } // end of MixedFraction default constructor
 
-    public MixedFraction(int numerator, int denominator, int wholeNumber) {
-        super(numerator, denominator);
+    public MixedFraction(int wholeNumber, Fraction fraction) {
+        super(fraction.getNumerator(), fraction.getDenominator());
         this.wholeNumber = wholeNumber;
     } // end of MixedFraction default constructor
 
@@ -37,12 +37,11 @@ public class MixedFraction extends Fraction {
     } // end of getWholeNumber accessor method
 
     /**
-     * Converts the mixed
-     * @return
+     * Concatenates the whole number, numerator, and denominator of Fraction.
+     * @return fraction as String
      */
     public String toString() {
-        String mixedFraction = getWholeNumber() + " " + getNumerator() + "/" + getDenominator();
-        return mixedFraction;
+        return getWholeNumber() + " " + getNumerator() + "/" + getDenominator();
     } // end of toString method
 
     /**
@@ -51,12 +50,87 @@ public class MixedFraction extends Fraction {
      * @return mixed fraction converted to decimal
      */
     public double toDouble() {
-        double reducedFraction = (double) (getDenominator() * getDenominator()) + getNumerator();
-        return reducedFraction;
+        return (double) (getNumerator() / getDenominator()) + getWholeNumber();
     } // end of toDouble method
 
+    public Fraction convertToImproper(MixedFraction mixedFraction) {
+        int wholeNumber = getWholeNumber();
+        int numerator = mixedFraction.getNumerator();
+        int denominator = mixedFraction.getDenominator();
+        numerator = denominator * wholeNumber + numerator;
+
+        return new Fraction(numerator,denominator);
+    } // emd of convertToImproper method
+
+    public MixedFraction convertToMixedNumbers(Fraction improperFraction) {
+        int wholeNumber = 0;
+        int numerator = improperFraction.getNumerator();
+        int denominator = improperFraction.getDenominator();
+
+        wholeNumber = numerator / denominator;
+        numerator = numerator % denominator;
+
+        Fraction fraction = new Fraction(numerator, denominator);
+        MixedFraction mixedNumbers = new MixedFraction(wholeNumber, fraction);
+
+        return mixedNumbers;
+    } // end of convertToMixedNumbers method
+
+    /**
+     * This method was made on 4 March 2023 by ROXAS, Johan Rickardo.
+     * Adds the Fraction1 addend and Fraction2 addend.
+     * Method overridden from FractionArithmetic.
+     * @param addend Fraction 2
+     * @return simplified sum of Fraction 1 and Fraction 2.
+     */
     @Override
     public Fraction add(Fraction addend) {
         return super.add(addend);
     } // end of overridden add method
+
+    protected MixedFraction add(MixedFraction addend, Fraction fractionAddend) {
+        int sumWholeNumber = this.wholeNumber + addend.getWholeNumber();
+        Fraction sumFraction = add(fractionAddend);
+        MixedFraction sum = new MixedFraction(sumWholeNumber, sumFraction);
+        return sum;
+    } // end of add method
+
+    @Override
+    protected Fraction subtract(Fraction subtrahend) {
+        return super.subtract(subtrahend);
+    } // end of overridden subtract method
+
+    protected MixedFraction subtract(MixedFraction subtrahend, Fraction fractionSubtrahend) {
+        int differenceWholeNumber = this.wholeNumber - subtrahend.getWholeNumber();
+        Fraction differenceFraction = subtract(fractionSubtrahend);
+        MixedFraction difference = new MixedFraction(differenceWholeNumber, differenceFraction);
+        return difference;
+    } // end of subtract method
+
+    @Override
+    protected Fraction multiplyBy(Fraction multiplicand) {
+        return super.multiplyBy(multiplicand);
+    } // end of overridden multiplyBy method
+
+    protected MixedFraction multiplyBy(MixedFraction multiplier, MixedFraction multiplicand) {
+        Fraction multiplierFraction = convertToImproper(multiplier);
+        Fraction multiplicanFraction = convertToImproper(multiplicand);
+        multiplierFraction.
+    } // edn of multiplyBy method
+
+    @Override
+    protected Fraction divideBy(Fraction dividend) {
+        return super.divideBy(dividend);
+    } // end of overridden divideBy method
+
+    /*
+    protected MixedFraction divideBy(MixedFraction dividend, Fraction fractionDividend) {
+        int quotientWholeNumber = this
+    }
+    */
+
+    @Override
+    public Fraction reduce() {
+        return super.reduce();
+    } // end of overridden reduce method
 } // end of class MixedFraction
